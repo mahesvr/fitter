d01bedfcfc01fc0873e646af0bd70185aa0c0aad
mahesvr <35959023+mahesvr@users.noreply.github.com>
2022-11-06 13:04:19 +0530

    Add files via upload

---
+++ js/demojs.js
@@ -0,0 +1,13 @@
+// var dt = new Date();
+//document.getElementById("datetime").innerHTML = dt.toLocaleString();
+
+const form =document.getElementById("form");
+const mstname =document.getElementById("fname");
+const age =document.getElementById("age");
+const pincode =document.getElementById("pin");
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    })
+
+    console.log(mstname);
